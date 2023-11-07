d14bbf00cb7e4398886f999f8aa74c0c7a6beeb2
donndewey <donn@deweydata.io>
2023-11-07 15:18:32 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='deweypy',
+    version='0.1.0',
+    packages=['deweypy'],
+    url='https://www.deweydata.io/',
+    license='',
+    author='Dewey Data Inc.',
+    author_email='info@deweydata.io',
+    description='Dewey Data Inc. Python lib'
+)
