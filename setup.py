966821ee9c559836e399f7ac62ae5c2b80db011a
donndewey <donn@deweydata.io>
2023-11-08 20:47:09 -0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='deweydatapy',
-    version='0.1.3',
+    version='0.1.4',
     packages=['deweydatapy'],
     url='https://www.deweydata.io/',
     license='',
