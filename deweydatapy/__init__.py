1b872dea496ac4dce9d1c1a49bde5a2e275b5a64
donndewey <donn@deweydata.io>
2023-11-08 20:23:03 -0800

    test upload

--- deweydatapy/__init__.py
+++ deweydatapy/__init__.py
@@ -1,1 +1,8 @@
-from .download import *
+from .download import (get_file_list,
+                       read_sample_data,
+                       read_sample_data0,
+                       read_local_data,
+                       download_files,
+                       download_files0,
+                       slice_files_df
+                       )
