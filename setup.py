b1360b7ea25262d42add48aa3c3b7b4e4f77ddf9
donndewey <donn@deweydata.io>
2023-11-12 15:13:09 -0800

    update

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     author_email='info@deweydata.io',
     description='Dewey Data Inc. Python Library',
     project_urls={
-        'Documentation': '',
-        'Funding': 'None',
+        'Documentation': 'https://community.deweydata.io/t/bulk-data-downloading-in-python-api-v3/26560',
         'Source': 'https://github.com/Dewey-Data/deweydatapy',
         'Tracker': 'https://community.deweydata.io/c/help/python/43',
     }
