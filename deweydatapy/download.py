99ecf924e5c8222d5a6e9c8102c1d027a0f73c64
donndewey <donn@deweydata.io>
2024-01-26 17:09:09 -0800

    v0.2.0 update

--- deweydatapy/download.py
+++ deweydatapy/download.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 import pandas as pd
 import requests
+from datetime import datetime
 
 def __make_api_endpoint(path):
     if not path.startswith("https://"):
@@ -13,8 +14,75 @@ def __make_api_endpoint(path):
     else:
         return path
 
+def get_meta(apikey, product_path, print_meta=True):
+    """
+    Collects the meta information from data server.
+
+    :param apikey: API Key.
+    :param product_path: API endpoint or Product ID.
+    :param print_meta: Print meta information. Default is True.
+    :return: A DataFrame object contains meta information.
+    """
+    product_path = __make_api_endpoint(product_path)
+    try:
+        response = requests.get(url=product_path+"/metadata",
+                                headers={'X-API-KEY': apikey,
+                                         'accept': 'application/json'})
+    except Exception as e:
+        print("Error in requests.get")
+        print(e)
+        print(" ")
+        return None
+
+    if response is None:
+        return None
+    elif response.status_code == 401:
+        print(response)
+        return None
+
+    res_json = response.json()
+
+    if 'total_files' not in res_json:
+        print("Error in response.json")
+        print(res_json)
+        print(" ")
+        return None
+
+    meta = res_json
+    # total_size is in mega bytes
+    meta['total_size'] = meta['total_size'] / 1000000
+    # Rename total_size to total_size_MB
+    meta['total_size_MB'] = meta.pop('total_size')
+
+    # Convert res_json to a DataFrame
+    # meta = pd.DataFrame(res_json, index=[0])
+    # # total_size is in mega bytes
+    # meta['total_size'] = meta['total_size'] / 1000000
+    # # change total_size to total_size_MB
+    # meta.rename(columns={'total_size': 'total_size_MB'}, inplace=True)
+
+    # meta = pd.DataFrame({
+    #     'total_files': [res_json['total_files']],
+    #     'total_pages': [res_json['total_pages']],
+    #     'total_size': [res_json['total_size'] / 1000000],
+    #     'expires_at': [res_json['expires_at']]
+    # })
+
+    if print_meta:
+        print(" ")
+        print("Metadata summary ------------------------------------------------")
+        print("Total number of files: {:,}".format(meta['total_files']))
+        print("Total files size (MB): {:,}".format(round(meta['total_size_MB'], 2)))
+        print(f"Date aggregation: {meta['partition_aggregation']}")
+        print(f"Date partition column: {meta['partition_column']}")
+        print(f"Data min available date: {meta['min_partition_key']}")
+        print(f"Data max available date: {meta['max_partition_key']}")
+        print("-----------------------------------------------------------------")
+        print(" ")
+    return meta
+
 def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
-                  partition_key_after=None, partition_key_before=None,
+                  start_date=None, end_date=None,
                   print_info=True):
     """
     Collects the file list information from data server.
@@ -23,34 +91,38 @@ def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
     :param product_path: API endpoint or Product ID.
     :param start_page: Start page of file list. Default is 1.
     :param end_page: End page of file list. Default is Inf.
-    :param partition_key_after: Partition key after in the form of '2021-07-01'. Default is None ('1000-01-01') which indicates no limit.
-    :param partition_key_before: Partition key before in the form of '2023-08-21'. Default is None ('9999-12-31') which indicates no limit.
+    :param start_date: Data start date character for files in the form of '2021-07-01'. Default is None ("1000-01-01"), which indicates no limit.
+    :param end_date: Data end date character for files in the form of '2023-08-21'. Default is None ('9999-12-31'), which indicates no limit.
     :param print_info: Print file list information. Default is True.
     :return: A DataFrame object contains files information.
     """
-    # apikey = apikey_
-    # product_path = pp_renthub
-    # product_path = pp_sg_poipoly
-    # start_page = 1
-    # end_page = float('inf')
-
-    if partition_key_after is None:
-        partition_key_after = "1000-01-01"
-    if partition_key_before is None:
-        partition_key_before = "9999-12-31"
 
     product_path = __make_api_endpoint(product_path)
+    meta = get_meta(apikey, product_path, print_meta=False)
     data_meta = None
     page_meta = None
     files_df = None
 
+    if start_date is None:
+        start_date = "1000-01-01"
+    if end_date is None:
+        end_date = "9999-12-31"
+
+    # To proper date format: for example '2023-3-4' to '2023-03-04'
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').strftime('%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').strftime('%Y-%m-%d')
+
     page = start_page
     while True:
+        if(meta['partition_column'] is None):
+            params_ = {'page': page}
+        else:
+            params_ = {'page': page,
+                       'partition_key_after': start_date,
+                       'partition_key_before': end_date}
         try:
             response = requests.get(url=product_path,
-                                    params={'page': page,
-                                            'partition_key_after': partition_key_after,
-                                            'partition_key_before': partition_key_before},
+                                    params=params_,
                                     headers={'X-API-KEY': apikey,
                                              'accept': 'application/json'})
         except Exception as e:
@@ -76,7 +148,7 @@ def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
             data_meta = pd.DataFrame({
                 'total_files': [res_json['total_files']],
                 'total_pages': [res_json['total_pages']],
-                'total_size': [res_json['total_size'] / 1000000],
+                'total_size_MB': [res_json['total_size'] / 1000000],
                 'expires_at': [res_json['expires_at']]
             })
 
@@ -87,7 +159,7 @@ def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
             pd.DataFrame({
                 'page': [res_json['page']],
                 'number_of_files_for_page': [res_json['number_of_files_for_page']],
-                'avg_file_size_for_page': [res_json['avg_file_size_for_page'] / 1000000],
+                'avg_file_size_for_page_MB': [res_json['avg_file_size_for_page'] / 1000000],
                 'partition_column': [res_json['partition_column']]
             })], ignore_index=True)
 
@@ -113,10 +185,10 @@ def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
 
     if print_info:
         print("\nFiles information summary ---------------------------------------")
-        print(f"Total number of pages: {data_meta['total_pages'].values[0]}")
-        print(f"Total number of files: {data_meta['total_files'].values[0]}")
-        print(f"Total files size (MB): {round(data_meta['total_size'].values[0], 2)}")
-        print(f"Average single file size (MB): {round(page_meta['avg_file_size_for_page'].mean(), 2)}")
+        print("Total number of pages: {:,}".format(data_meta['total_pages'].values[0]))
+        print("Total number of files: {:,}".format(data_meta['total_files'].values[0]))
+        print("Total files size (MB): {:,}".format(round(data_meta['total_size_MB'].values[0], 2)))
+        print("Average single file size (MB): {:,}".format(round(page_meta['avg_file_size_for_page_MB'].mean(), 2)))
         print(f"Date partition column: {page_meta['partition_column'].values[0]}")
         print(f"Expires at: {data_meta['expires_at'].values[0]}")
         print("-----------------------------------------------------------------\n")
@@ -124,8 +196,7 @@ def get_file_list(apikey, product_path, start_page=1, end_page=float('inf'),
 
     return files_df
 
-
-def read_sample_data(url, nrows=100):
+def read_sample(url, nrows=100):
     """
     Read sample data into memory from a URL.
 
@@ -151,9 +222,12 @@ def read_sample_data(url, nrows=100):
 
     return (df)
 
+# Backward compatibility
+read_sample_data = read_sample
+
 
 # Read first file data into memory
-def read_sample_data0(apikey, product_path, nrows=100):
+def read_sample0(apikey, product_path, nrows=100):
     """
     Read first file data into memory using API key and product path.
     :param apikey: API key.
@@ -169,21 +243,11 @@ def read_sample_data0(apikey, product_path, nrows=100):
     else:
         return None
 
-
-def read_local_data(path, nrows=None):
-    """
-    Read local data into memory from a path
-
-    :param path: Path to a .csv.gz file.
-    :param nrows: Number of rows to read. Default is None (all).
-    :return: A DataFrame object contains data.
-    """
-    df = pd.read_csv(path, nrows=nrows)
-    return df
-
+# Backward compatibility
+read_sample_data0 = read_sample0
 
 # Download files from file list to a destination folder
-def download_files(files_df, dest_folder, filename_prefix="", skip_exists=True):
+def download_files(files_df, dest_folder, filename_prefix=None, skip_exists=False):
     """
     Download files from file list to a destination folder.
 
@@ -197,6 +261,9 @@ def download_files(files_df, dest_folder, filename_prefix="", skip_exists=True):
     if (not (dest_folder.endswith("/"))):
         dest_folder = dest_folder + "/"
 
+    if filename_prefix is None:
+        filename_prefix = ""
+
     files_df.reset_index(drop=True, inplace=True)
 
     # number of files
@@ -209,7 +276,8 @@ def download_files(files_df, dest_folder, filename_prefix="", skip_exists=True):
         dest_path = dest_folder + file_name
 
         if os.path.exists(dest_path) and skip_exists:
-            print(f"File {dest_path} already exists. Skipping...")
+            print(f"File already exists: {dest_path}")
+            print(f"Skipping...")
             continue
 
         print(f"Writing {dest_path}")
@@ -222,21 +290,28 @@ def download_files(files_df, dest_folder, filename_prefix="", skip_exists=True):
         sys.stdout.flush()
 
 
-def download_files0(apikey, product_path, dest_folder, filename_prefix=None, skip_exists=True):
+def download_files0(apikey, product_path, dest_folder,
+                    start_date=None, end_date=None,
+                    filename_prefix=None, skip_exists=False):
     """
     Download files with API key and product path to a destination folder.
 
     :param apikey: API Key.
     :param product_path: API endpoint or Product ID.
     :param dest_folder: Destination local folder to save files.
+    :param start_date: Data start date character for files in the form of '2021-07-01'. Default is None ("1000-01-01"), which indicates no limit.
+    :param end_date: Data end date character for files in the form of '2023-08-21'. Default is None ('9999-12-31'), which indicates no limit.
     :param filename_prefix: Prefix for file names.
     :param skip_exists: Prefix for file names. Skips downloading if the file exists. Default is True.
     :return:
     """
-    files_df = get_file_list(apikey, product_path, print_info=True)
+    files_df = get_file_list(apikey, product_path,
+                             start_page=1, end_page=float('inf'),
+                             start_date=start_date, end_date=end_date,
+                             print_info=True)
     print("   ")
 
-    if files_df is not None and len(files_df) > 0:
+    if files_df is not None and files_df.shape[0] > 0:
         download_files(files_df, dest_folder, filename_prefix, skip_exists)
 
 
@@ -258,3 +333,18 @@ def slice_files_df(files_df, start_date, end_date=None):
                              (files_df['partition_key'] <= end_date)]
 
     return sliced_df
+
+
+def read_local(path, nrows=None):
+    """
+    Read local data into memory from a path
+
+    :param path: Path to a .csv.gz or csv file.
+    :param nrows: Number of rows to read. Default is None (all).
+    :return: A DataFrame object contains data.
+    """
+    df = pd.read_csv(path, nrows=nrows)
+    return df
+
+# Backward compatibility
+read_local_data = read_local
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+# from setuptools import setup
 
 setup(
     name='deweydatapy',
-    version='0.1.4',
+    version='0.2.0',
     packages=['deweydatapy'],
     url='https://www.deweydata.io/',
     license='',
@@ -10,7 +11,7 @@ setup(
     author_email='info@deweydata.io',
     description='Dewey Data Inc. Python Library',
     project_urls={
-        'Documentation': 'https://community.deweydata.io/t/bulk-data-downloading-in-python-api-v3/26560',
+        'Documentation': 'https://github.com/Dewey-Data/deweydatapy',
         'Source': 'https://github.com/Dewey-Data/deweydatapy',
         'Tracker': 'https://community.deweydata.io/c/help/python/43',
     }
